a145841e8af7d5992dee660a75c06330db691b80
Sinol <103750421+Sinol95@users.noreply.github.com>
2023-04-12 19:19:46 +0200

    Add files via upload
    Changement CSS bouton déconnexion

--- header/header.php
+++ header/header.php
@@ -2,11 +2,5 @@
 <link rel="shortcut icon" href="/img/lama_icon.webp">
 <header>
     <img id="lama_logo" src="../img/lama_logo.webp">
-    <?php 
-        if ($_SESSION["user_id"] != null && isset($_SESSION["user_id"])){
-            echo "<a href='/login/deconnexion.php' id='connexion_button'>Déconnexion</a>";
-        }else{
-            echo "<a href='/login/login.php' id='connexion_button'>Déconnexion</a>";
-        }
-    ?>
+    <a href='/login/deconnexion.php' id='connexion_button'>Déconnexion</a>
 </header>
