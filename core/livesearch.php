603996d332a37fa3bb7f0cfcde8e00b9d713bd9e
Sinol <103750421+Sinol95@users.noreply.github.com>
2023-04-12 11:17:03 +0200

    Add files via upload
    Tout est beau

--- core/livesearch.php
+++ core/livesearch.php
@@ -1,48 +1,49 @@
 <?php
-session_start();
-$mysqli = new mysqli("localhost", "lama", "lama_admin", "lama");
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-if (isset($_POST["input"])){
-    
-    $result = $mysqli->execute_query("SELECT * from User WHERE User.name LIKE '{$_POST["input"]}%' OR User.user_id LIKE '{$_POST["input"]}%'");
-    if (mysqli_num_rows($result) > 0){
-        foreach ($result as $res) {
-            if(file_exists("../img/user_profile_pictures/".$res["user_id"].".webp")){
-                $img=$res["user_id"];
+    session_start();
+    $mysqli = new mysqli("localhost", "lama", "lama_admin", "lama");
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    if (isset($_POST["input"])){
+        
+        $result = $mysqli->execute_query("SELECT * from User WHERE User.name LIKE '{$_POST["input"]}%' OR User.user_id LIKE '{$_POST["input"]}%'");
+        if (mysqli_num_rows($result) > 0){
+            foreach ($result as $res) {
+                if(file_exists("../img/user_profile_pictures/".$res["user_id"].".webp")){
+                    $img=$res["user_id"];
+                }
+                else{
+                    $img="default";
+                }
+                printf("<div class='friend_block'>
+                <img src='../img/user_profile_pictures/%s.webp'>
+                <div>
+                    <b>%s</b>
+                    <br>
+                    <i>@%s</i>
+                    <button onclick='request(event)'>Ajouter</button>
+                </div>
+                </div>", $img, $res["name"], $res["user_id"]);
             }
-            else{
-                $img="default";
-            }
-            printf("<div class='friend_block'>
-            <img src='../img/user_profile_pictures/%s.webp'>
-            <div>
-                <b>%s</b>
-                <br>
-                <i>@%s</i>
-                <button onclick='request(event)'>Ajouter</button>
-            </div>
-            </div>", $img, $res["name"], $res["user_id"]);
+        }else{
+            echo "<h6 style='text-align: center;'>No data found</h6>";
         }
-    }else{
-        echo "<h6 style='text-align: center;'>No data found</h6>";
     }
-}
 ?>
-<script type="text/javascript">   
-function request(event){
-    let request = new XMLHttpRequest();
-    var data = new FormData();
-    let user_id_2 = event.target.parentNode.children[2].innerHTML.split('@')[1];    
-    data.append("user_id_2", user_id_2);
-    data.append("name", event.target.parentNode.children[0].innerHTML);
-    request.open("POST", '/core/request.php', true);
-    request.onreadystatechange = () => {
-        // In local files, status is 0 upon success in Mozilla Firefox
-        if (request.readyState === XMLHttpRequest.DONE) {
-            event.target.parentNode.style.display = "none";
-            event.target.parentNode.parentNode.style.display = "none";
-        }
-    };
-    request.send(data);
-}
+
+<script type="text/javascript">
+    function request(event) {
+        let request = new XMLHttpRequest();
+        var data = new FormData();
+        let user_id_2 = event.target.parentNode.children[2].innerHTML.split('@')[1];
+        data.append("user_id_2", user_id_2);
+        data.append("name", event.target.parentNode.children[0].innerHTML);
+        request.open("POST", '/core/request.php', true);
+        request.onreadystatechange = () => {
+            // In local files, status is 0 upon success in Mozilla Firefox
+            if (request.readyState === XMLHttpRequest.DONE) {
+                event.target.parentNode.style.display = "none";
+                event.target.parentNode.parentNode.style.display = "none";
+            }
+        };
+        request.send(data);
+    }
 </script>
--- core/request.php
+++ core/request.php
@@ -1,7 +1,7 @@
 <?php
-session_start();
-$mysqli = new mysqli("localhost", "lama", "lama_admin", "lama");
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$mysqli->execute_query("INSERT INTO `Request` (user_id_1,user_id_2) VALUES (?,?)",[$_POST["user_id_2"], $_SESSION["user_id"]]);
-echo $_POST["name"];
+    session_start();
+    $mysqli = new mysqli("localhost", "lama", "lama_admin", "lama");
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    $mysqli->execute_query("INSERT INTO `Request` (user_id_1,user_id_2) VALUES (?,?)",[$_POST["user_id_2"], $_SESSION["user_id"]]);
+    echo $_POST["name"];
 ?>
